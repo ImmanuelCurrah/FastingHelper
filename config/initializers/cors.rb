fed3f02bc87e8e7c1a96d58d1a539e497c8758db
Immanuel Currah <immanueli.currah@gmail.com>
2022-02-24 18:47:27 -0500

    trying to deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://enigmatic-mesa-82837.herokuapp.com"
+    origins "*"
 
     resource "*",
       headers: :any,
