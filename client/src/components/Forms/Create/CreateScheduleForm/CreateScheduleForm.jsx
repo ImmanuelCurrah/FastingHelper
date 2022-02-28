6aecf21c33b81bab62907c22020bd4d3725bd1cf
Immanuel Currah <immanueli.currah@gmail.com>
2022-02-28 10:37:52 -0500

    create schedule form

--- client/src/components/Forms/Create/CreateScheduleForm/CreateScheduleForm.jsx
+++ client/src/components/Forms/Create/CreateScheduleForm/CreateScheduleForm.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import { useForm } from "../../../../hooks/useForm";
+import { createSchedule } from "../../../../services/routes/ScheduleRoutes/scheduleRoutes";
+import { useParams } from "react-router-dom";
 
 export default function CreateScheduleForm() {
-  return <div>CreateScheduleForm</div>;
+  const defaultInput = {
+    name: "",
+    start_date: "",
+    end_date: "",
+  };
+
+  const { id } = useParams();
+  const { handleChange, form, setForm } = useForm(defaultInput);
+
+  const onSubmit = async (e) => {
+    const newSchedule = await createSchedule(id, form);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>name</label>
+      <br />
+      <input type="text" id="name" value={form.name} onChange={handleChange} />
+      <br />
+      <label>start date</label>
+      <br />
+      <input
+        type="date"
+        id="start_date"
+        value={form.start_date}
+        onChange={handleChange}
+      />
+      <br />
+      <label>end date</label>
+      <br />
+      <input
+        type="date"
+        id="end_date"
+        value={form.end_date}
+        onChange={handleChange}
+      />
+      <button>create</button>
+    </form>
+  );
 }
