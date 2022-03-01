8e1099b1860d5213b31950719df6eacf7883a151
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-01 14:48:25 -0500

    some styling on the schedule page

--- client/src/components/Forms/Create/CreateScheduleForm/CreateScheduleForm.jsx
+++ client/src/components/Forms/Create/CreateScheduleForm/CreateScheduleForm.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "../../../../hooks/useForm";
 import { createSchedule } from "../../../../services/routes/ScheduleRoutes/scheduleRoutes";
 import { useParams } from "react-router-dom";
+import classes from "./CreateScheduleForm.module.css";
 
 export default function CreateScheduleForm() {
   const defaultInput = {
@@ -19,29 +20,41 @@ export default function CreateScheduleForm() {
 
   return (
     <form onSubmit={onSubmit}>
-      <label>name</label>
-      <br />
-      <input type="text" id="name" value={form.name} onChange={handleChange} />
-      <br />
-      <label>start date</label>
-      <br />
-      <input
-        type="date"
-        id="start_date"
-        value={form.start_date}
-        onChange={handleChange}
-      />
-      <br />
-      <label>end date</label>
-      <br />
-      <input
-        type="date"
-        id="end_date"
-        value={form.end_date}
-        onChange={handleChange}
-      />
-      <br />
-      <button>create</button>
+      <div className={classes.input_container}>
+        <label className={classes.label}>name</label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          value={form.name}
+          onChange={handleChange}
+        />
+        <br />
+      </div>
+      <div className={classes.input_container}>
+        <label className={classes.label}>start date</label>
+        <br />
+        <input
+          type="date"
+          id="start_date"
+          value={form.start_date}
+          onChange={handleChange}
+        />
+        <br />
+      </div>
+      <div className={classes.input_container}>
+        <label className={classes.label}>end date</label>
+        <br />
+        <input
+          type="date"
+          id="end_date"
+          value={form.end_date}
+          onChange={handleChange}
+        />
+        <br />
+      </div>
+
+      <button className={classes.button}>create</button>
     </form>
   );
 }
