a5413acae0fc873d9ec4f542277da21ffd631649
Immanuel Currah <immanueli.currah@gmail.com>
2022-03-01 18:32:19 -0500

    timer styling done

--- client/src/components/Timer/Timer.jsx
+++ client/src/components/Timer/Timer.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Countdown from "react-countdown";
-// import classes from "./Timer.module.css";
+import classes from "./Timer.module.css";
 
 export default function Timer() {
   const [timer, setTimer] = useState(0);
@@ -9,6 +9,7 @@ export default function Timer() {
   return (
     <div>
       <select
+        className={classes.select}
         onChange={(e) => {
           setTimer(+e.target.value);
           setToggle(true);
@@ -18,13 +19,21 @@ export default function Timer() {
         <option value={28800000}>8 hour daily fast</option>
         <option value={86400000}>24 hour day fast</option>
       </select>
-      <div> {!toggle && "00:00:00:00"}</div>
+      <div className={classes.timer_num}> {!toggle && "00:00:00:00"}</div>
       <br />
       {toggle && (
-        <Countdown date={Date.now() + timer}>
+        <Countdown className={classes.timer_num} date={Date.now() + timer}>
           <h2>completed</h2>
         </Countdown>
       )}
+      <button
+        onClick={() => {
+          setToggle(false);
+        }}
+        className={classes.button}
+      >
+        Reset
+      </button>
     </div>
   );
 }
